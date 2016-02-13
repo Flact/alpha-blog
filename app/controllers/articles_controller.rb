daf3a938e70df2f1606f7f5dbf8be1fef797f171
Atom Ghost <watiya@outlook.com>
2016-02-13 06:00:12 +0000

    create template, actions and routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,18 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    
+    def create
+        #render plain: params[:article].inspect
+        @article = Article.new(ar_prms)
+        @article.save
+        redirect_to articles_show(@article)
+    end
+    
+    
+    private
+    
+    def ar_prms
+       params.require(:article).permit(:title, :description) 
+    end
 end
