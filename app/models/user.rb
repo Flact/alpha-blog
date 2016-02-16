4eb40aa4a0d612121ced652378e0f7f780af16f6
Atom Ghost <watiya@outlook.com>
2016-02-16 16:19:40 +0000

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
+REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25},format: {with: REGEX}
 
 end
