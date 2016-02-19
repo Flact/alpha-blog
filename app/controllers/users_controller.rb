105887a16fb3e5767f9a643757261ba2f64f3ceb
Atom Ghost <watiya@outlook.com>
2016-02-19 06:01:43 +0000

    Add edit update fasility with form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,24 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
       
       if @user.save
-        flash[:success] = "Welcome to Blog #{@user.username}"
+        flash[:success] = "Welcome to the Blog, #{@user.username}"
         redirect_to articles_path
       else
         render :new
+      end
+    end
+    
+    def edit
+      @user = User.find(params[:id])
+    end
+    
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success] = "Your account was updated successfully"
+        redirect_to articles_path
+      else
+        
       end
     end
     
