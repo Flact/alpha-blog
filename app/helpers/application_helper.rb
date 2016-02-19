b40f9618ae14745f0f1760b3242e72a01db79225
Atom Ghost <watiya@outlook.com>
2016-02-19 14:34:04 +0000

    Create show action and gravater image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def gravater_for(user, options = {size: val})
+    
+    size = options[:size]
+    gravater_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravater_url = "https://secure.gravatar.com/avatar/#{gravater_id}?s=#{size}"
+    image_tag(gravater_url, alt: user.username, class: "img-circle")
+    
+  end
 end
