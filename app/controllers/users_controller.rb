6292152c43e41abdb28f703b6b8db7db631c23f5
Atom Ghost <watiya@outlook.com>
2016-02-20 11:29:08 +0000

    Add user listig and show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
         @user = User.new
     end
     
+    def index
+      @users = User.all
+    end
+    
     def create
       @user = User.new(user_params)
       
@@ -28,7 +32,7 @@ class UsersController < ApplicationController
         flash[:success] = "Your account was updated successfully"
         redirect_to articles_path
       else
-        
+        render :edit
       end
     end
     
